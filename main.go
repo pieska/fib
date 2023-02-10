29164c8b4acd159d3557e7e6bf9a5171f31c386e
Andreas Piesk <a.piesk@mailbox.org>
2023-02-10 23:49:59 +0100

    var

--- main.go
+++ main.go
@@ -9,12 +9,14 @@ import (
 
 func main() {
 
-	n := flag.Uint("n", 5, "Stellen der Fibonacci-Zahl")
+	var n uint
+
+	flag.UintVar(&n, "n", 5, "Stellen der Fibonacci-Zahl")
 	flag.Parse()
 
-	fib := fib.Generate(*n)
+	fib := fib.Generate(n)
 
-	fmt.Printf("fibs(%d) = %v\n", *n, fib)
+	fmt.Printf("fibs(%d) = %v\n", n, fib)
 
 	os.Exit(0)
 }
