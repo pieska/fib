50c5831faf681434f9e6d8e327ae4be3f35496b2
Andreas Piesk <a.piesk@mailbox.org>
2023-04-06 10:44:56 +0200

    explicit declaratio

--- main.go
+++ main.go
@@ -11,21 +11,23 @@ func main() {
 
 	var n uint
 	var i bool
+	var fibs_slice []uint
+	var fibs_chan <-chan uint
 
 	flag.UintVar(&n, "n", 5, "Anzahl der Fibonacci-Zahlen")
 	flag.BoolVar(&i, "i", false, "immediate output, verwendet Channel")
 	flag.Parse()
 
 	if !i {
-		fibs := fib.GenerateSlice(n)
+		fibs_slice = fib.GenerateSlice(n)
 
-		fmt.Printf("fibs(%d) = %v\n", n, fibs)
+		fmt.Printf("fibs(%d) = %v\n", n, fibs_slice)
 	} else {
-		fibs := fib.GenerateChannel(n)
+		fibs_chan = fib.GenerateChannel(n)
 
 		fmt.Printf("fibs(%d) =  ", n)
 
-		for fib := range fibs {
+		for fib := range fibs_chan {
 			fmt.Printf("%d ", fib)
 		}
 		fmt.Println()
